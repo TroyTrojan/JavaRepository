2335e8038b5120a68c1c82a9015ccb6527ab00ee
TroyTrojan <135703624+TroyTrojan@users.noreply.github.com>
2024-11-04 20:49:25 +0800

    2024/11/4

--- Java Practise-Black Horse/day11/src/test/it/大小写转换.java
+++ Java Practise-Black Horse/day11/src/test/it/大小写转换.java
@@ -3,16 +3,19 @@ package test.it;
 import java.util.Scanner;
 
 public class 大小写转换 {
-  Scanner sc = new Scanner(System.in);
-  int money;
-  while(true){
-      System.out.println("请输入一个金额");
-        money = sc.nextInt();
-      if(money>=0&&money<=9999999){
-          break;
-      }else{
-          System.out.println("金额无效");
-      }
-    }
-  String[] arr={"壹","贰","叁","肆","伍","陆","柒","捌","玖",};
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int money;
+		while (true) {
+			System.out.println("请输入一个金额");
+			money = sc.nextInt();
+			if (money >= 0 && money <= 9999999) {
+				break;
+			} else {
+				System.out.println("金额无效");
+			}
+		}
+		String[] arr = {"壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖",};
+	}
 }
+
